25e57649c938686e271550335b14afec63d5ffd8
Topias Peiponen <topiaspe@metropolia.fi>
2020-09-17 13:45:59 +0300

    Added imgfragment class and fragment to layout

---
+++ app/src/main/java/com/example/ar_lab_large/ImgFragment.kt
@@ -0,0 +1,18 @@
+package com.example.ar_lab_large
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.ar.sceneform.ux.ArFragment
+
+class ImgFragment : ArFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        return view
+    }
+}
--- app/src/main/java/com/example/ar_lab_large/MainActivity.kt
+++ app/src/main/java/com/example/ar_lab_large/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.ar_lab_large
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
+
+/**
+ * @author Topias Peiponen, Roope Vaarama
+ * @since 17.09.2020
+ */
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragment : ArFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
     }
 }
